432076d93009093e039990e21ba38f5cb2577210
LuxARTS <bacelolucas@gmail.com>
2020-08-13 22:18:13 -0300

    Add goCreateVersion

--- internal/commands/createversion.go
+++ internal/commands/createversion.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"asimov-tool-cli/internal/utils"
+	"bytes"
 	"fmt"
-	"os"
+	"os/exec"
 	"regexp"
 
 	"github.com/urfave/cli"
@@ -11,8 +13,6 @@ import (
 const (
 	ProjectUnknown = iota
 	ProjectGo
-	ProjectNodeJS
-	ProjectC
 )
 
 func CreateVersion(c *cli.Context) {
@@ -24,20 +24,41 @@ func CreateVersion(c *cli.Context) {
 	version := c.Args().First()
 
 	if !isValidVersion(version) {
-		fmt.Println("Incorrect version format.\nThe format must be mayor.minor.patch.\n")
+		fmt.Println("Incorrect version format.\nThe format must be mayor.minor.patch.")
+		return
+	}
+
+	featureName, err := getFeatureName()
+	if err != nil {
+		fmt.Println("Error getting the feature name.")
+		return
+	}
+
+	filename := version + "-" + featureName
+	fmt.Println("Creating version " + filename + "...")
+
+	if utils.FileExist("builds/" + filename) {
+		fmt.Println("The version already exists.\nPlease specify a different version number.")
 		return
 	}
 
 	projectType := getProjectType()
+
 	switch projectType {
 	case ProjectGo:
 		fmt.Println("Go project detected.")
+		err = createGoVersion(filename)
 	case ProjectUnknown:
 		fmt.Println("Unknown project type.")
 		return
 	}
 
-	fmt.Printf("Creating version %s\n", version)
+	if err != nil {
+		fmt.Printf("Fail to create version.\nError: %v\n", err)
+		return
+	}
+
+	fmt.Println("Version created successfully!")
 }
 
 func isValidVersion(v string) bool {
@@ -52,9 +73,35 @@ func isValidVersion(v string) bool {
 }
 
 func getProjectType() int {
-	if _, err := os.Stat("go.mod"); err == nil {
+	if utils.FileExist("go.mod") {
 		return ProjectGo
 	}
 
 	return ProjectUnknown
+}
+
+func getFeatureName() (string, error) {
+	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	re := regexp.MustCompile(`feature/(.+)`)
+	results := re.FindStringSubmatch(out.String())
+
+	return results[1], nil
+}
+
+func createGoVersion(filename string) error {
+	//os.Setenv("GOOS", "linux")
+	//os.Setenv("GOARCH", "arm")
+	//os.Setenv("GOARM", "7")
+	cmd := exec.Command("go", "build", "-o", "builds/"+filename, "cmd/main.go")
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
