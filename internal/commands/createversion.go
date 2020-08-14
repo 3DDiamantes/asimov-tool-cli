9beccaa273eb78e557331ba2e85ddc8a3ecc8018
LuxARTS <bacelolucas@gmail.com>
2020-08-13 21:18:24 -0300

    Add check version

---
+++ internal/commands/createversion.go
@@ -0,0 +1,51 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/urfave/cli"
+)
+
+const (
+	ProjectUnknown = iota
+	ProjectGo
+	ProjectNodeJS
+	ProjectC
+)
+
+func CreateVersion(c *cli.Context) {
+	if !c.Args().Present() {
+		fmt.Println("Version number must be specified.")
+		return
+	}
+
+	version := c.Args().First()
+
+	if !isValidVersion(version) {
+		fmt.Println("Incorrect version format.\nThe format must be mayor.minor.patch.\n")
+		return
+	}
+
+	fmt.Printf("Creating version %s\n", version)
+}
+
+func isValidVersion(v string) bool {
+	re := regexp.MustCompile(`^(\d+)\.(\d+)\.(\d+)$`)
+
+	results := re.FindString(v)
+
+	if results != "" {
+		return true
+	}
+	return false
+}
+
+func getProjectType() int {
+	if _, err := os.Stat("go.mod"); err == nil {
+		return ProjectGo
+	}
+
+	return ProjectUnknown
+}
